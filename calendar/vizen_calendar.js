41609241f98792ec19876ee496b499c28b6e5989
zpzgzerg <zpzgzerg@naver.com>
2012-02-03 18:41:53 +0900

    달력 작업 계속 진행중

--- calendar/vizen_calendar.js
+++ calendar/vizen_calendar.js
@@ -10,9 +10,10 @@
 		click_btn		: "",
 		contents		: "",
 		currentDate		: new Date(),
-		nowDate			: null,		
-		prevDate		: null,
-		nextDate		: null 
+		nowDate			: new Date(),		
+		prevDate		: new Date(),
+		nextDate		: new Date(),
+		days 			: new Array("일", "월", "화", "수", "목", "금", "토")
 		
 	};	
 	
@@ -22,81 +23,161 @@
 		vizenConfig.id = id;
 		vizenConfig.click_btn = click_btn;
 				
-		var dates = {
-			year 	: vizenConfig.currentDate.getFullYear(),
-			month 	: vizenConfig.currentDate.getMonth()+1,
-			day 	: vizenConfig.currentDate.getDay()
-		};		
+		var dates = vizenConfig.currentDate;
 		
 		// 현재보고 있는 날짜
-		vizenConfig.nowDate = new Date(dates.year, dates.month, dates.day);
+		vizenConfig.nowDate = new Date(dates.getFullYear(), dates.getMonth(), 1);
 		
 		// 현재보고 있는 기준에서 이전 날짜
-		vizenConfig.prevDate = new Date(dates.year, dates.month-1, 1);
+		vizenConfig.prevDate = new Date(dates.getFullYear(), dates.getMonth()-1, 1);
 		
 		// 현재보고 있는 기준에서 다음 날짜
-		vizenConfig.nextDate = new Date(dates.year, dates.month+1, 1);		
+		vizenConfig.nextDate = new Date(dates.getFullYear(), dates.getMonth()+1, 1);		
 		
 		// div 생성
 		$(document.body).append("<div id='"+vizenConfig.id+"'><div>");
 		
+		// 달력 그리기
 		drawCalendar();
-		$("#"+id).hide();
+		drawContents();
+		
+		$("#"+vizenConfig.id).hide();
 		
 		$("#"+vizenConfig.click_btn).click(function() {
-			$("#"+id).show();
+			$("#"+vizenConfig.id).show();
 		});
 		
 		// 이전 월 클릭시
 		$("#prev_btn").click(function() {			
 			
-			var prevDates = {
-				year 	: vizenConfig.prevDate.getFullYear(),
-				month 	: vizenConfig.prevDate.getMonth(),
-				day 	: vizenConfig.prevDate.getDay()
-			};
-			
 			// 현재보고 있는 날짜
-			vizenConfig.nowDate = new Date(prevDates.year, prevDates.month, prevDates.day);
+			vizenConfig.nowDate = new Date(vizenConfig.prevDate.getFullYear(), vizenConfig.prevDate.getMonth(), 1);
 			
 			// 현재보고 있는 기준에서 이전 날짜
-			vizenConfig.prevDate = new Date(prevDates.year, prevDates.month-1, 1);
+			vizenConfig.prevDate = new Date(vizenConfig.prevDate.getFullYear(), vizenConfig.prevDate.getMonth()-1, 1);
 			
 			// 현재보고 있는 기준에서 다음 날짜
-			vizenConfig.nextDate = new Date(prevDates.year, prevDates.month+1, 1);	
+			vizenConfig.nextDate = new Date(vizenConfig.prevDate.getFullYear(), vizenConfig.prevDate.getMonth()+1, 1);	
+			
+			$("#txtYear2").text(vizenConfig.nowDate.getFullYear());
+			$("#txtMonth").text(vizenConfig.nowDate.getMonth()+1);			
 								
-			drawCalendar();
+			drawContents();
 		});
 		
 		// 다음 월 클릭시
 		$("#next_btn").click(function() {			
 			
-			var nextDates = {			
-				year 	: vizenConfig.nextDate.getFullYear(),
-				month 	: vizenConfig.nextDate.getMonth()+1,
-				day 	: vizenConfig.nextDate.getDay()			
-			};			
-			
 			// 현재보고 있는 날짜
-			vizenConfig.nowDate = new Date(nextDates.year, nextDates.month, nextDates.day);
+			vizenConfig.nowDate = new Date(vizenConfig.nextDate.getFullYear(), vizenConfig.nextDate.getMonth(), 1);			
 			
 			// 현재보고 있는 기준에서 이전 날짜
-			vizenConfig.prevDate = new Date(nextDates.year, nextDates.month-1, 1);
+			vizenConfig.prevDate = new Date(vizenConfig.nextDate.getFullYear(), vizenConfig.nextDate.getMonth()-1, 1);			
 			
 			// 현재보고 있는 기준에서 다음 날짜
-			vizenConfig.nextDate = new Date(nextDates.year, nextDates.month+1, 1);	
+			vizenConfig.nextDate = new Date(vizenConfig.nextDate.getFullYear(), vizenConfig.nextDate.getMonth()+1, 1);			
 			
-			drawCalendar();		
-		});		
+			$("#txtYear2").text(vizenConfig.nowDate.getFullYear());
+			$("#txtMonth").text(vizenConfig.nowDate.getMonth()+1);
+			
+			drawContents();		
+		});	
+		
+		$("#close_btn").click(function() {
+			$("#"+vizenConfig.id).hide();
+		});	
 	
 	}		
 	
 	function drawContents() {
 		
+		// 이번달 마지막 날짜 구하기
+		var lastDate = new Date(vizenConfig.nextDate.getFullYear(), vizenConfig.nextDate.getMonth(), vizenConfig.nextDate.getDate()-1);				
+		
 		var contents = "";
 		
+		contents += "<table width='100%' border='0' cellspacing='0' cellpadding='0' class='cdTable'>";
+		contents += "	<thead>";
+		contents += "		<tr>";
+		contents += "			<th scope='col'>"+vizenConfig.days[0]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[1]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[2]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[3]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[4]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[5]+"</th>";
+		contents += "			<th scope='col'>"+vizenConfig.days[6]+"</th>";
+		contents += "		</tr>";
+		contents += "	</thead>";
+		contents += "	<tbody>";
+		contents += "		<tr>";
 		
+		for (var i=0; i<vizenConfig.nowDate.getDay(); i++) {
+			contents += "		<td class='cd'></td>";			
+		}
 		
+		for(var i=1; i<=lastDate.getDate(); i++) {		
+
+			if(new Date(vizenConfig.nowDate.getFullYear(), vizenConfig.nowDate.getMonth(), i).getDay() == 0 && i != 1) {
+
+				contents += "</tr>"
+				contents += "<tr>"					
+				
+			}
+			
+			contents += "		<td class='cdck'><a href='#'>"+i+"</a></td>";			
+		}
+		
+		contents += "		</tr>";
+		
+		/*contents += "		<tr>";
+		contents += "			<td class='sun cd'><a href='#'>29</a></td>";
+		contents += "			<td class='cd'>30</td>";
+		contents += "			<td class='cd'>31</td>";
+		contents += "			<td class='cdck'><a href='#'>1</a></td>";
+		contents += "			<td class='cdck'>2</td>";
+		contents += "			<td class='cdck'>3</td>";
+		contents += "			<td class='cdck'>4</td>";
+		contents += "		</tr>";
+		contents += "		<tr>";
+		contents += "			<td class='cdck'>5</td>";
+		contents += "			<td class='cdck'>6</td>";
+		contents += "			<td class='cdck'>7</td>";
+		contents += "			<td class='cdck'>8</td>";
+		contents += "			<td class='cdck'>9</td>";
+		contents += "			<td class='cdck'>10</td>";
+		contents += "			<td class='sat'>11</td>";
+		contents += "		</tr>";
+		contents += "		<tr>";
+		contents += "			<td class='sun'>12</td>";
+		contents += "			<td>13</td>";
+		contents += "			<td>14</td>";
+		contents += "			<td>15</td>";
+		contents += "			<td>16</td>";
+		contents += "			<td>17</td>";
+		contents += "			<td class='sat'>18</td>";
+		contents += "		</tr>";
+		contents += "		<tr>";
+		contents += "			<td class='sun'>19</td>";
+		contents += "			<td>20</td>";
+		contents += "			<td>21</td>";
+		contents += "			<td>22</td>";
+		contents += "			<td>23</td>";
+		contents += "			<td>24</td>";
+		contents += "			<td class='sat'>25</td>";
+		contents += "		</tr>";
+		contents += "		<tr>";
+		contents += "			<td class='sun'>26</td>";
+		contents += "			<td>27</td>";
+		contents += "			<td>28</td>";
+		contents += "			<td>29</td>";
+		contents += "			<td class='cd'>1</td>";
+		contents += "			<td class='cd'>2</td>";
+		contents += "			<td class='sat cd'>3</td>";
+		contents += "		</tr>";*/
+		contents += "	</tbody>";
+		contents += "</table>";
+		
+		$("#Contents").html(contents);		
 	}
 
 	// 달력 인터페이스 생성
@@ -116,7 +197,7 @@
 		vizenConfig.contents += "						까지";
 		vizenConfig.contents += "						&nbsp;<img src='img/calendar_hit_btn.gif' align='absmiddle'>";
 		vizenConfig.contents += "					</td>";
-		vizenConfig.contents += "					<td align='right' valign='top'><img src='img/calendar_btn_close.gif'></td>";
+		vizenConfig.contents += "					<td align='right' valign='top'><img src='img/calendar_btn_close.gif' id='close_btn' style='cursor:hand;'></td>";
 		vizenConfig.contents += "				</tr>";
 		vizenConfig.contents += "			</table>";
 		vizenConfig.contents += "		</td>";
@@ -210,77 +291,17 @@
 		vizenConfig.contents += "									<table width='250' cellpadding='0' cellspacing='0' border='0'>";
 		vizenConfig.contents += "										<tr>";
 		vizenConfig.contents += "											<td width='18' valign='top' height='22'><img src='img/calendar_btn_back.gif' id='prev_btn' style='cursor:pointer'></td>";
-		vizenConfig.contents += "											<td valign='top' class='calyear' width='214'>"+vizenConfig.nowDate.getFullYear()+"년 "+vizenConfig.nowDate.getMonth()+"월</td>";
+		vizenConfig.contents += "											<td valign='top' class='calyear' width='214'><span id='txtYear2'>"+vizenConfig.nowDate.getFullYear()+"</span>년 <span id='txtMonth'>"+(vizenConfig.nowDate.getMonth()+1)+"</span>월</td>";
 		vizenConfig.contents += "											<td width='18' valign='top'><img src='img/calendar_btn_next.gif' id='next_btn' style='cursor:pointer'></td>";
 		vizenConfig.contents += "										</tr>";
 		vizenConfig.contents += "										<tr>";
 		vizenConfig.contents += "											<td colspan='3' height='2' bgcolor='#82868b'></td>";
-		vizenConfig.contents += "										</tr>";
-		vizenConfig.contents += "										<div id='Contents'></div>";		
+		vizenConfig.contents += "										</tr>";		
 		vizenConfig.contents += "										<tr>";
 		vizenConfig.contents += "											<td colspan='3' valign='top'>";
-		vizenConfig.contents += "												<table width='100%' border='0' cellspacing='0' cellpadding='0' class='cdTable'>";
-		vizenConfig.contents += "													<thead>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<th scope='col'>일</th>";
-		vizenConfig.contents += "															<th scope='col'>월</th>";
-		vizenConfig.contents += "															<th scope='col'>화</th>";
-		vizenConfig.contents += "															<th scope='col'>수</th>";
-		vizenConfig.contents += "															<th scope='col'>목</th>";
-		vizenConfig.contents += "															<th scope='col'>금</th>";
-		vizenConfig.contents += "															<th scope='col'>토</th>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "													</thead>";
-		vizenConfig.contents += "													<tbody>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<td class='sun cd'><a href='#'>29</a></td>";
-		vizenConfig.contents += "															<td class='cd'>30</td>";
-		vizenConfig.contents += "															<td class='cd'>31</td>";
-		vizenConfig.contents += "															<td class='cdck'><a href='#'>1</a></td>";
-		vizenConfig.contents += "															<td class='cdck'>2</td>";
-		vizenConfig.contents += "															<td class='cdck'>3</td>";
-		vizenConfig.contents += "															<td class='cdck'>4</td>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<td class='cdck'>5</td>";
-		vizenConfig.contents += "															<td class='cdck'>6</td>";
-		vizenConfig.contents += "															<td class='cdck'>7</td>";
-		vizenConfig.contents += "															<td class='cdck'>8</td>";
-		vizenConfig.contents += "															<td class='cdck'>9</td>";
-		vizenConfig.contents += "															<td class='cdck'>10</td>";
-		vizenConfig.contents += "															<td class='sat'>11</td>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<td class='sun'>12</td>";
-		vizenConfig.contents += "															<td>13</td>";
-		vizenConfig.contents += "															<td>14</td>";
-		vizenConfig.contents += "															<td>15</td>";
-		vizenConfig.contents += "															<td>16</td>";
-		vizenConfig.contents += "															<td>17</td>";
-		vizenConfig.contents += "															<td class='sat'>18</td>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<td class='sun'>19</td>";
-		vizenConfig.contents += "															<td>20</td>";
-		vizenConfig.contents += "															<td>21</td>";
-		vizenConfig.contents += "															<td>22</td>";
-		vizenConfig.contents += "															<td>23</td>";
-		vizenConfig.contents += "															<td>24</td>";
-		vizenConfig.contents += "															<td class='sat'>25</td>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "														<tr>";
-		vizenConfig.contents += "															<td class='sun'>26</td>";
-		vizenConfig.contents += "															<td>27</td>";
-		vizenConfig.contents += "															<td>28</td>";
-		vizenConfig.contents += "															<td>29</td>";
-		vizenConfig.contents += "															<td class='cd'>1</td>";
-		vizenConfig.contents += "															<td class='cd'>2</td>";
-		vizenConfig.contents += "															<td class='sat cd'>3</td>";
-		vizenConfig.contents += "														</tr>";
-		vizenConfig.contents += "													</tbody>";
-		vizenConfig.contents += "												</table>";
+		vizenConfig.contents += "												<div id='Contents'></div>";
 		vizenConfig.contents += "											</td>";
-		vizenConfig.contents += "										</tr>";
+		vizenConfig.contents += "										</tr>";		
 		vizenConfig.contents += "									</table>";
 		vizenConfig.contents += "								</td>";
 		vizenConfig.contents += "							</tr>";
@@ -305,6 +326,6 @@
 		vizenConfig.contents += "	</tr>";
 		vizenConfig.contents += "</table>";		
 						
-		$("#"+vizenConfig.id).html(vizenConfig.contents);		
+		$("#"+vizenConfig.id).html(vizenConfig.contents);
 				
 	}	
